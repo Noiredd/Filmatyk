8080ed7a26c0322b4b7a2c05f26192e5b6cf0fd3
Noiredd <snowball91b@gmail.com>
2018-12-10 19:53:50 +0100

    more intelligent setup for Windows

--- setup.bat
+++ setup.bat
@@ -1,4 +1,13 @@
 cd fw-local
-python3 -m pip install requests_html
-python3 -m pip install matplotlib
-python3 -m pip install pillow
+rem For some users, python3.exe would not exist.
+rem In this case, use a fall-back python.exe and hope that it's version 3.6+
+where python3
+if $errorlevel$ neq 0 (
+  set python=python
+) else (
+  set python=python3
+)
+echo %python%
+%python% -m pip install requests_html
+%python% -m pip install matplotlib
+%python% -m pip install pillow
